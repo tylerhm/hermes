5d19f9b15a5ec682e3f9f064e8f4acc7b82b4a83
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-05 18:47:17 -0500

    Set up event listening

--- src/renderer/Home.tsx
+++ src/renderer/Home.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from 'react';
 import './Home.css';
 
 export default function Home() {
+  const [sourceName, setSourceName] = useState<string>('');
+  const [inputName, setInputName] = useState<string>('');
+  const [outputName, setOutputName] = useState<string>('');
+
+  useEffect(() => {
+    const updateFileName = (key: string, name: string) => {
+      switch (key) {
+        case 'source':
+          setSourceName(name);
+          break;
+        case 'input':
+          setInputName(name);
+          break;
+        case 'output':
+          setOutputName(name);
+          break;
+        default:
+          console.error('Invalid file classifier');
+      }
+    };
+
+    window.electron.ipcRenderer.on('main-file-selected', updateFileName);
+
+    return () => {
+      window.electron.ipcRenderer.removeListener(
+        'main-file-selected',
+        updateFileName
+      );
+    };
+  }, []);
+
   return (
     <div className="container">
       <div className="buttonGroup">
