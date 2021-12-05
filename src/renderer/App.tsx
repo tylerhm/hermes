96628f4292853a9894b1361efa89c08303225db7
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-05 18:39:26 -0500

    Added renderer->electron global interface in App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,19 @@ import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 
+declare global {
+  interface Window {
+    electron: {
+      ipcRenderer: {
+        on: (listener: string, func: any) => void;
+        removeListener: (listener: string, func: any) => void;
+        myPing: () => void;
+        setFile: (key: string) => void;
+      };
+    };
+  }
+}
+
 export default function App() {
   return (
     <Router>
