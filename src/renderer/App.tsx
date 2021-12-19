6f276d4d76e28467ae5466714d29f62834cfea91
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-12-19 13:34:17 -0500

    Dependency checker

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import DependencyCheck from './DependencyCheck';
 import Home from './Home';
 import './App.css';
 
@@ -9,6 +10,7 @@ declare global {
         on: (listener: string, func: unknown) => void;
         once: (listener: string, func: unknown) => void;
         removeListener: (listener: string, func: unknown) => void;
+        checkDeps: () => void;
         setFile: (key: string, isDirectory: boolean) => void;
         setTimeLimit: (limit: number) => void;
         judge: () => void;
@@ -21,7 +23,8 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" exact component={DependencyCheck} />
+        <Route path="/home" component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/renderer/DependencyCheck.tsx
@@ -0,0 +1,94 @@
+import { useHistory } from 'react-router';
+import { Button, List, Spin, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import CHANNELS from './channels';
+import eventHandler from './eventHandler';
+
+type DepsStatusType = {
+  python3: boolean;
+  apollo: boolean;
+};
+
+/*
+ * Render a splash screen to check dependencies
+ * Only allow user to proceed if deps are found
+ */
+const DependencyCheck = () => {
+  const history = useHistory();
+
+  const [depsStatus, setDepsStatus] = useState<DepsStatusType>({
+    python3: false,
+    apollo: false,
+  });
+  const [checking, setChecking] = useState<boolean>(true);
+
+  const getMissingDeps = () => {
+    const missingDeps: Array<string> = [];
+    Object.entries(depsStatus).forEach(([dep, exists]) => {
+      if (!exists) missingDeps.push(dep);
+    });
+    return missingDeps;
+  };
+
+  const checkDeps = () => {
+    setChecking(true);
+    eventHandler.checkDeps();
+  };
+
+  useEffect(() => {
+    checkDeps();
+
+    const updateDeps = (newDepsStatus: DepsStatusType) => {
+      if (Object.values(newDepsStatus).every((value) => value))
+        history.push('/home');
+
+      setChecking(false);
+      setDepsStatus(newDepsStatus);
+    };
+
+    eventHandler.on(CHANNELS.DEPS_CHECKED, updateDeps);
+
+    return () => {
+      eventHandler.removeListener(CHANNELS.DEPS_CHECKED, updateDeps);
+    };
+  }, [history]);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+        padding: '1em',
+      }}
+    >
+      {checking ? (
+        <>
+          <h2 style={{ marginBottom: '3em' }}>Detecing dependencies...</h2>
+          <Spin />
+        </>
+      ) : (
+        <>
+          <List
+            header={<h2>Missing dependencies</h2>}
+            dataSource={getMissingDeps()}
+            renderItem={(item) => (
+              <List.Item>
+                <Typography.Text>{item}</Typography.Text>
+              </List.Item>
+            )}
+            style={{ marginBottom: '3em' }}
+          />
+          <Button type="primary" onClick={checkDeps}>
+            Detect
+          </Button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DependencyCheck;
