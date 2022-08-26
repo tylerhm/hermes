f050913b49e485773a19db878679042016a3b206
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-08-26 00:50:01 -0400

    Conditionally render cpp lang selector

--- src/renderer/components/CppStandardSelectorRow.tsx
+++ src/renderer/components/CppStandardSelectorRow.tsx
@@ -6,22 +6,42 @@ import { CppStandardType, StoreKeyType } from '../utils/types';
 import DropdownSelectorRow from './DropdownSelectorRow';
 
 const CPP_STANDARD_OPTIONS: Array<CppStandardType> = [11, 14, 17, 20];
+const CPP_EXTENSIONS = ['cpp', 'cc', 'C'];
+
+const checkCppLang = (source: string) => {
+  return CPP_EXTENSIONS.includes(source.split('.').pop() ?? '');
+};
 
 // Row with dropdown to select checker
 const CppStandardSelector = () => {
   const [cppStandard, setCppStandard] = useState<CppStandardType>(17);
+  const [isCppLang, setIsCppLang] = useState<boolean>(false);
 
   // Handle incoming information from existing store
   const foundInStore = (key: StoreKeyType, res: unknown) => {
     if (key === 'cpp-standard') setCppStandard(res as CppStandardType);
+    else if (key === 'source') setIsCppLang(checkCppLang(res as string));
+  };
+
+  // If we select a new source, update our lang accordingly
+  const checkAndUpdateCppLang = (key: StoreKeyType, res: unknown) => {
+    if (key === 'source') setIsCppLang(checkCppLang(res as string));
   };
 
   // Ask for all information from store, and listen on file selections
   useEffect(() => {
-    const remover = eventHandler.on(CHANNELS.FOUND_IN_STORE, foundInStore);
+    const removers: Array<() => void> = [];
+
+    removers.push(eventHandler.on(CHANNELS.FOUND_IN_STORE, foundInStore));
+    removers.push(
+      eventHandler.on(CHANNELS.FILE_SELECTED, checkAndUpdateCppLang)
+    );
+
     eventHandler.requestFromStore('cpp-standard');
+    eventHandler.requestFromStore('source');
+
     return () => {
-      remover();
+      removers.forEach((remover) => remover());
     };
   }, []);
 
@@ -30,7 +50,7 @@ const CppStandardSelector = () => {
     eventHandler.setCppStandard(newCppStandard);
   };
 
-  return (
+  return isCppLang ? (
     <Space direction="vertical" style={{ width: '100%' }}>
       <DropdownSelectorRow
         label="C++ Standard"
@@ -41,7 +61,7 @@ const CppStandardSelector = () => {
         }
       />
     </Space>
-  );
+  ) : null;
 };
 
 export default CppStandardSelector;
